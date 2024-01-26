b9d5c6819d81e93b4f587fc52a06b3ae0c0a3cf3
Diaco Soltanpour <dyaco8464@yahoo.com>
2024-01-26 15:52:52 +0000

    activated pytest

--- setup.py
+++ setup.py
@@ -3,41 +3,41 @@ import os
 from setuptools import find_packages, setup
 
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-bom',
-    version='1.233',
+    name="django-bom",
+    version="1.233",
     packages=find_packages(),
     include_package_data=True,
-    license='GPL 3.0 License',
-    description='A simple Django app to manage a bill of materials.',
+    license="GPL 3.0 License",
+    description="A simple Django app to manage a bill of materials.",
     long_description=README,
-    long_description_content_type='text/markdown',
-    url='https://www.indabom.com/',
-    author='Mike Kasparian',
-    author_email='mpkasp@gmail.com',
+    long_description_content_type="text/markdown",
+    url="https://github.com/s-diaco/django-bom",
+    author="Mike Kasparian",
+    author_email="dyaco8464@yahoo.com",
     classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 3.1',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 3.1",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
     install_requires=[
-        'python-social-auth',
-        'social-auth-app-django',
-        'google-api-python-client',
-        'django-materializecss-form',
-        'django-money',
+        "python-social-auth",
+        "social-auth-app-django",
+        "google-api-python-client",
+        "django-materializecss-form",
+        "django-money",
     ],
 )
